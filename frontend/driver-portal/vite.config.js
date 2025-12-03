1653b4264b23d8e4ec60aba8bf9f481c7f4802c1
ACherniske <apcherniske@gmail.com>
2025-12-03 07:17:38 -0500

    fix: deployment script for both apps and landing

--- frontend/driver-portal/vite.config.js
+++ frontend/driver-portal/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/ECEG301/driver/' : '/',
   plugins: [
     react(),
     VitePWA({
@@ -18,8 +19,8 @@ export default defineConfig({
         background_color: '#ffffff',
         display: 'standalone',
         orientation: 'portrait',
-        scope: '/',
-        start_url: '/',
+        scope: process.env.VITE_BASE_PATH || '/',
+        start_url: process.env.VITE_BASE_PATH || '/',
         icons: [
           {
             src: '/vite.svg',
